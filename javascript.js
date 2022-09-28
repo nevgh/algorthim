b027431810a1f9dbcf763d671ec94a1d02d2a26f
nevgh <114558561+nevgh@users.noreply.github.com>
2022-09-28 18:15:19 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,6 +8,18 @@ return names.indexOf(element) === index;
 
 console.log(newNames);
 
+/* Method 2* /
+let name = ["nevean", "ninos", "sargon", "nevean", "ninos", "jay", "ninos", "jay"];
+let newNames = [];
+
+
+for ( var i = 0; i<name.length; i++) {
+if (!newNames.includes(name[i])) {
+newNames.push(name[i]);}
+}
+
+console.log(newNames);
+
 
 
 /* two */ 
