ea972fc7fbffdde55581802889f29df084ea56af
Nevean Ghatas <nevean-gh@hotmail.com>
2022-10-01 15:14:57 -0400

    testing the clone

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ return names.indexOf(element) === index;
 });
 
 console.log(newNames);
-
+// something changed here asdfasdfasdfasdfa
 /* Method 2*/
 let name = ["nevean", "ninos", "sargon", "nevean", "ninos", "jay", "ninos", "jay"];
 let newNames = [];
